bbeebe8ea5d5dfc403c532cbdbab64fefe8a0fe7
aannsss <138268039+aannsss@users.noreply.github.com>
2023-09-25 23:43:51 +0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,5 @@
+n=1
+print(n)
+print(n+2,n+2,sep=' ')
+print(n+4,n+4,n+4, sep=' ')
+print(n+6,n+6,n+6,n+6,sep=' ')
---
+++ ex5.py
@@ -0,0 +1,2 @@
+x=6
+print('Daca Ion spune',x,',atunci Vasile spune',x-2,x-1,x,x+1,x+2)
